739f7902b20d7af188b1b4dd197d2e0fc1d8a4e1
Arjun Cosare <arjun.cosare@outlook.com>
2021-04-30 13:26:35 -0600

    character length restrictions

--- models/phonebook.js
+++ models/phonebook.js
@@ -5,11 +5,13 @@ const phoneBookSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
+        minlength: 3,
         unique: true
     },
     number: {
         type: String,
-        required: true
+        required: true,
+        minlength: 8
     }
 });
 
