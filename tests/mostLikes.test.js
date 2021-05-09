dc354d5f79e3169c4c93d06ffb43848ec1adfb21
Arjun Cosare <arjun.cosare@outlook.com>
2021-05-09 14:20:33 -0600

    mostLikes function with tests

---
+++ tests/mostLikes.test.js
@@ -0,0 +1,76 @@
+const mostLikes = require("../utils/list_helper").mostLikes;
+
+describe('most likes', () => {
+    const blogs = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'James W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'James W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        }
+    ];
+
+    const oneBlog = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'James W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        }
+    ]
+
+    test('should return most likes among the blogs', () => {
+        expect(mostLikes(blogs)).toEqual({author: "Edsger W. Dijkstra", likes: 20});
+    });
+
+    test('should return the only author', () => {
+        expect(mostLikes(oneBlog)).toEqual({author: "James W. Dijkstra", likes: 5});
+    });
+    test('should return empty with no blogs', () => {
+        expect(mostLikes([])).toEqual({});
+    });
+});
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -7,15 +7,15 @@ const totalLikes = blogs => {
 }
 
 const favoriteBlog = blogs => {
-    const mostLikedBlog =  blogs.filter((blog) => blog.likes === Math.max(...blogs.map(el => el.likes)));
-    if(mostLikedBlog.length === 0) return null;
+    const mostLikedBlog = blogs.filter((blog) => blog.likes === Math.max(...blogs.map(el => el.likes)));
+    if (mostLikedBlog.length === 0) return null;
     return mostLikedBlog[0];
 }
 
 const mostBlogs = blogs => {
-    const authors  = new Set(blogs.map(blog => blog.author));
+    const authors = new Set(blogs.map(blog => blog.author));
     let authorsAndBlogs = [];
-    
+
 
     authors.forEach((author) => {
         const authorAndBlog = {
@@ -24,12 +24,31 @@ const mostBlogs = blogs => {
         };
 
         authorsAndBlogs.push(authorAndBlog);
-    });  
+    });
 
     const mostAuthorBlogged = authorsAndBlogs.filter(elem => elem.blogs === Math.max(...authorsAndBlogs.map(x => x.blogs)));
 
-    if(mostAuthorBlogged.length > 0) return authorsAndBlogs.filter(elem => elem.blogs === Math.max(...authorsAndBlogs.map(x => x.blogs)))[0]
+    if (mostAuthorBlogged.length > 0) return authorsAndBlogs.filter(elem => elem.blogs === Math.max(...authorsAndBlogs.map(x => x.blogs)))[0]
 
+    return {};
+}
+
+const mostLikes = blogs => {
+    const authors = new Set(blogs.map(blog => blog.author));
+
+    let authorAndLikes = [];
+
+    authors.forEach((author) => {
+        const authorDets = {
+            author,
+            likes: blogs.filter(el => el.author === author).reduce((prev, next) => prev + next.likes, 0)
+        };
+
+        authorAndLikes.push(authorDets);
+    });
+
+    const mostAuthorGainLikes = authorAndLikes.filter(elem => elem.likes === Math.max(...authorAndLikes.map(x => x.likes)));
+    if (mostAuthorGainLikes.length > 0) return mostAuthorGainLikes[0];
     return {};
 }
 
@@ -37,5 +56,6 @@ module.exports = {
     dummy,
     totalLikes,
     favoriteBlog,
-    mostBlogs
+    mostBlogs,
+    mostLikes
 }
